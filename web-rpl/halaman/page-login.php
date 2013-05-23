6c1113d3d8039cd1719dbf386949e1b9e400556e
zaki <zaki.fuadi@s1.informatika.unsyiah.ac.id>
2013-05-23 09:19:01 +0700

    saya upload file login yang baru

---
+++ web-rpl/halaman/page-login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0181)http://welcome.indonesiawifi.net/wifi.id/default/?switch_url=http://1.1.1.1/login.html&ap_mac=1c:e6:c7:85:f7:10&client_mac=70:f1:a1:d5:2a:77&wlan=@wifi.id&redirect=www.google.co.id/ -->
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Pragma" content="no-cache">  
+<title>Selamat datang | Wifi.Id</title>
+<link rel="stylesheet" type="text/css" href="../css/login.css" media="all">
+<script type="text/javascript" async="" src="../js/ga.js"></script>
+<script type="text/javascript" async="" src="../js/em.js"></script>
+<script type="text/javascript" async="" src="../js/ga(1).js"></script>
+<script type="text/javascript" src="../js/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" src="../js/modal.popup.js"></script>
+
+
+
+<script type="text/javascript" async="" src="js/em_js"></script></head>
+<body onload="loadAction();">
+<div style="position:absolute;top:580px;left:960px;">
+	<img src="../images/logo-zad.png" height="100px" width="400px">
+</div>
+<div id="wraper">
+	<div id="container">
+		<div id="content">
+		<div class="login">
+		<form method="post" id="loginForm" action="../proses.php">
+			<h1>User Login</h1>
+			<div class="warp_from">
+				<select id="pilihan" name="akses">
+					<option value="">Pilih Akses</option>
+					<option value="0">Proyek</option>
+					<option value="1">Jurusan</option>
+					<option value="2">User</option>
+				</select>
+				<div class="tbl_login"><p>Username</p><input type="text" name="username_" id="username"></div>
+				<input type="hidden" name="username" id="username_hidden">
+				<div class="clear"></div>
+				<div class="tbl_login"><p>Password</p><input type="password" id="password" name="password"></div>
+				<div class="clear"></div>
+			</div>
+			<input type="submit" class="tombol" name="Submit" value="Login">
+		</form>
+</body></html>
