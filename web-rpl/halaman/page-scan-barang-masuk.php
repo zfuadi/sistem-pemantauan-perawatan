2096aa44c53513a22f8fb893788d11f4784153d9
Musanna <Musanna.adnan@s1.informatika.unsyiah.ac.id>
2013-05-29 12:33:52 +0700

    baru

---
+++ web-rpl/halaman/page-scan-barang-masuk.php
@@ -0,0 +1,47 @@
+<?php session_start();?>
+<html>
+<head>
+<title>Sistem Informasi Pemantauan Perawatan</title>
+<link rel="stylesheet" href="../css/style.css" type="text/css"/>
+<link rel="stylesheet" href="../css/nav.css" type="text/css"/>
+<head>
+<body>
+<table class="table_utama" width="1000" border="0" align="center" cellpadding="0" cellspacing="0">
+	<tr height="30"><td colspan="2" id="top"><div id="welcome">Selamat Datang, Sipulan<br><a href="logout.php">Logout</a></div></td></tr>
+<tr><td colspan="2" ><img src="../images/header.jpg" width="1000" height="200"><?php //echo "header_web";?></td></tr>
+<tr height="30"><td colspan="2" class="td_head"><?php include('text-run.php');?></tr>
+<tr height="400" valign="top">
+<td class="td_menu">
+	<?php 
+		include('sidebar.php');
+	?>	
+<?php //include('halaman/tampil_menu.php');?>
+<?php //include('halaman/tampil_artikel.php');?>
+<?php //include('halaman/buku_tamu.php');?>
+
+
+</td>
+<td class="isi">
+	<?php 
+		include('navigation.php');
+		//include('page-home.php');
+	?>
+	<div class="post">
+	Scan barang masuk
+	</div>
+	<div class="content">
+		<table align="center" valign="midle">
+			<tr>
+				<td>
+					<input type="submit" name="submit" value="S C A N" style="width:200;height:50;font-size:20;font-weight:bold;padding:5px;position:absolute;top:500;left:690;">
+				</td>
+			</tr>
+		</table>
+	</div>
+	
+</td>
+</tr>
+<tr><td colspan="2" class="td_foot"><?php include('footer.php');?></tr>
+</table>
+</body>
+</html>
---
+++ web-rpl/halaman/sidebar.php
@@ -0,0 +1,10 @@
+<html>
+		<div class="title">Photo Profile
+			<div class="foto"><?php include('foto.php'); ?></div>
+		</div>
+		
+		<div class="title">Date and Time
+			<div class="foto"><object width="230" height="270" data="../data/calendar3.swf"></object></div>
+		</div>
+		
+</html>
